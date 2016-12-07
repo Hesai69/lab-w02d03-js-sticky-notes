84d7970a1db8e92f2599d429ae38a21f53543ee0
Qian Chen <oda_san@hotmail.com>
2016-12-07 14:04:52 -0800

    working

--- dom.js
+++ dom.js
@@ -1,1 +1,67 @@
- // JavaScript goes here
+createContainer();
+
+function createContainer(){
+  var container = document.createElement('div');
+  container.classList.add('container');
+  document.querySelector('#main').appendChild(container);
+  createButton();
+}
+
+function createButton(){
+  var button = document.createElement('button');
+  var div = document.createElement('div');
+  button.textContent = 'Add A Note';
+  button.classList.add('button');
+  div.classList.add('button');
+  div.appendChild(button);
+  document.querySelector('.container').appendChild(div);
+  button.addEventListener('click', createNote);
+}
+
+function createNote(evt){
+  var note = document.createElement('div');
+  var p = document.createElement('p');
+  var time = document.createElement('p');
+
+  note.classList.add('note');
+  time.classList.add('time');
+  time.textContent = timeStamp();
+  note.appendChild(time);
+  // Editable note <p>
+  p.setAttribute('contenteditable', 'true');
+  // p.classList.remove('time');
+  p.classList.add('noteText');
+  p.textContent = 'Edit this text';
+  note.appendChild(createRemove());
+  note.appendChild(p);
+  document.querySelector('.container').appendChild(note);
+  updateNotes(noteCounter());
+}
+
+function createRemove(){
+  var remove = document.createElement('h6');
+  var text = document.createElement('span');
+  text.classList.add('remove');
+  text.textContent = 'x';
+  remove.appendChild(text);
+  text.addEventListener('click', removeNote);
+  return remove;
+}
+
+function removeNote(evt){
+  evt.target.parentNode.parentNode.remove();
+  updateNotes(noteCounter());
+}
+
+function noteCounter(){
+  return document.querySelectorAll('.note').length;
+}
+
+function updateNotes(count){
+  var counter = document.querySelector('.counter');
+  counter.textContent = count;
+}
+
+function timeStamp(){
+  return Date().slice(0,21);
+}
